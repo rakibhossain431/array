e075054640ddde4ca3b0cacfa4e2d731f5387791
rakibhossain431 <rakibjp431@gmail.com>
2022-01-30 23:55:20 +0600

    array

---
+++ app.js
@@ -0,0 +1,51 @@
+
+// alert("Hello");
+
+// let names1 = ["rakib","rony","rag","robul"]
+// let names2 = ["rakib","rony","rag","robul"]
+
+// names3 = names1.concat(names2);
+
+// console.log(names3);
+// console.log(names3.length);
+
+
+
+//  let num = new Array();
+
+//  for(let i = 0; i<5; i++){
+//     num[i] =parseInt(prompt("Enter the any number:"));
+//  }
+// let sum = 0;
+// for(i =0; i<5; i++){
+    
+//     console.log(num[i]);
+
+//     sum = sum+num[i];
+
+// }
+// console.log(sum);
+
+// let num = [7,2,4,5,8];
+
+// console.log(num);
+// num.shift();
+// console.log(num);
+// num.unshift(12);
+// console.log(num);
+
+
+// num.splice(3,0,14,25);
+// console.log(num);
+
+//  let  numb = num.slice(2.4);
+// console.log(numb);
+
+let nam = ["rakib","hasan","roby", "anika","raju"]
+
+let sort = nam.sort();
+
+console.log(sort);
+ 
+let nam2 = sort.reverse();
+console.log(nam2);
